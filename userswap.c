d620ecdf071b9666de54592c116c2835701d1459
wincenttjoi <wincenttsjoi@gmail.com>
2021-11-02 23:19:35 +0800

    Implement userswap_alloc

--- userswap.c
+++ userswap.c
@@ -1,4 +1,7 @@
 #include "userswap.h"
+#include <sys/mman.h>
+#include <unistd.h>
+#include <signal.h>
 
 // This function sets the LORM to size.
 // If size is not a multiple of the page size, size should be rounded up to the next
@@ -13,15 +16,39 @@ void userswap_set_size(size_t size) {
 // This function should allocate size bytes of memory that is controlled by the
 // swap scheme described above in the “Controlled memory regions” section, and
 // return a pointer to the start of the memory.
-// If size is not a multiple of the page size, size should be rounded up to the next
-// multiple of the page size.
 // This function may be called multiple times without any intervening
 // userswap_free, i.e., there may be multiple memory allocations active at any
 // given time.
 // If the SIGSEGV handler has not yet been installed when this function is called,
 // then this function should do so. 
 void *userswap_alloc(size_t size) {
-  return NULL;
+  struct sigaction sa;
+  sa.sa_flags = SA_SIGINFO;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_sigaction = sigsegv_handler;
+  sigaction(SIGSEGV, &sa, NULL);
+
+  char *addr;
+  // If size is not a multiple of the page size, size should be rounded up to the next
+  // multiple of the page size.
+  long sizeForMmap = 0;
+  long pagesize = sysconf(_SC_PAGE_SIZE);
+  if (size <= pagesize) {
+    sizeForMmap = pagesize;
+  } else {
+    sizeForMmap = pagesize * 2;
+  }
+  addr = mmap(NULL, sizeForMmap, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
+  if (addr == MAP_FAILED) {
+    printf("Mapping failed");
+  }
+
+  return addr;
+}
+
+void sigsegv_handler(int signal, siginfo_t* info, void* context) {
+  printf("Signal %d received\n", signal);
+  abort();
 }
 
 // mem can be assumed to be a pointer previously returned by userswap_alloc
--- workload_wrfile.c
+++ workload_wrfile.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
 
   for (size_t i = 0; i < memory_size / sizeof(uintptr_t); ++i) {
     if (mem[i] != i) {
-      printf("Failed read at entry %zu, expected %zx, got %" PRIxPTR "\n", i, i, file_map[i]);
+      printf("Failed read at entry %zu, expected %zx, got %" PRIxPTR "\n", i, i, mem[i]);
       failed = 1;
     }
   }
